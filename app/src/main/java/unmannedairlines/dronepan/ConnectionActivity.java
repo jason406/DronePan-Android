85ab18360bf9c3379baab942ed433963f560f6e3
Dennis Baldwin <dennisbaldwin@gmail.com>
2016-12-27 17:07:13 -0600

    Reworking Android with SDK 3.5.1

---
+++ app/src/main/java/unmannedairlines/dronepan/ConnectionActivity.java
@@ -0,0 +1,66 @@
+package unmannedairlines.dronepan;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import dji.sdk.base.DJIBaseProduct;
+
+public class ConnectionActivity extends AppCompatActivity {
+
+    private static final String TAG = ConnectionActivity.class.getName();
+
+    private TextView mTextConnectionStatus;
+    private TextView mTextProduct;
+
+    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Toast.makeText(getApplicationContext(), "Connection status changed", Toast.LENGTH_LONG).show();
+
+
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
+                            Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
+                            Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
+                            Manifest.permission.READ_PHONE_STATE,
+                    }
+                    , 1);
+        }
+
+        setContentView(R.layout.activity_connection);
+
+        // Get notified of the device connection change
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DJIConnection.FLAG_CONNECTION_CHANGE);
+        registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+}
